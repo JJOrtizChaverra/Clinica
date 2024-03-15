a22f34f327bbd975e8e11761da5fef3f35d29279
JJOrtizChaverra <juanjochaverra02@gmail.com>
2024-03-15 17:51:55 -0500

    Creando la clase Connection y el metodo para hacer la conexion a la base de datos de manera local

---
+++ models/connection.php
@@ -0,0 +1,13 @@
+<?php
+
+class Connection {
+
+    public function connection() {
+
+        $database = new PDO("mysql:host=localhost;dbname=clinica;", "root", "");
+
+        $database -> exec("set names UTF8");
+
+        return $database;
+    }
+}
