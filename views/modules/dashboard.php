d69caf04d16894b7311ad7f2dd944208f1e4b453
JJOrtizChaverra <juanjochaverra02@gmail.com>
2024-03-15 17:43:10 -0500

    Aplicando algunos cambios en el dashboard, cambiando los titulos, colores e iconos de las cajas de contenido

--- views/modules/dashboard.php
+++ views/modules/dashboard.php
@@ -6,63 +6,63 @@
 
     <!-- Small boxes (Stat box) -->
     <div class="row">
-        <div class="col-lg-3 col-xs-6">
+        <div class="col-xs-12 col-sm-6 col-lg-3">
             <!-- small box -->
-            <div class="small-box bg-aqua">
+            <div class="small-box" style="background-color: #F781D8; color: white;">
                 <div class="inner">
-                    <h3>150</h3>
+                    <h3>Secretarias</h3>
 
-                    <p>New Orders</p>
+                    <p>Iniciar sesion</p>
                 </div>
                 <div class="icon">
-                    <i class="ion ion-bag"></i>
+                    <i class="fa fa-female"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="#" class="small-box-footer">Ingresar <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
+        <div class="col-xs-12 col-sm-6 col-lg-3">
             <!-- small box -->
-            <div class="small-box bg-green">
+            <div class="small-box" style="background-color: #BDBDBD; color: white;">
                 <div class="inner">
-                    <h3>53<sup style="font-size: 20px">%</sup></h3>
+                    <h3>Doctores</h3>
 
-                    <p>Bounce Rate</p>
+                    <p>Inicar sesion</p>
                 </div>
                 <div class="icon">
-                    <i class="ion ion-stats-bars"></i>
+                    <i class="fa fa-user-md"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="#" class="small-box-footer">Ingresar <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
+        <div class="col-xs-12 col-sm-6 col-lg-3">
             <!-- small box -->
             <div class="small-box bg-yellow">
                 <div class="inner">
-                    <h3>44</h3>
+                    <h3>Paciente</h3>
 
-                    <p>User Registrations</p>
+                    <p>Inicar sesion</p>
                 </div>
                 <div class="icon">
-                    <i class="ion ion-person-add"></i>
+                    <i class="fa fa-users"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="#" class="small-box-footer">Ingresar <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
+        <div class="col-xs-12 col-sm-6 col-lg-3">
             <!-- small box -->
             <div class="small-box bg-red">
                 <div class="inner">
-                    <h3>65</h3>
+                    <h3>Administrador</h3>
 
-                    <p>Unique Visitors</p>
+                    <p>Iniciar sesion</p>
                 </div>
                 <div class="icon">
-                    <i class="ion ion-pie-graph"></i>
+                    <i class="fa fa-male"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="#" class="small-box-footer">Ingresar <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <!-- ./col -->
