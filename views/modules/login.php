6df77493353fb87a266444388efdc167da46ec27
JJOrtizChaverra <juanjochaverra02@gmail.com>
2024-03-15 23:50:11 -0500

    Modificamos el mensaje de ingresar como y añadimos el codigo php para llamar al controlador de usuarios y hacer la validacion de login

--- views/modules/login.php
+++ views/modules/login.php
@@ -4,34 +4,35 @@
     </div>
     <!-- /.login-logo -->
     <div class="login-box-body">
-        <p class="login-box-msg">Ingresar como
-            <?php
-
-            if (isset($_GET["url"])) {
-                if (isset($_GET["secretary"])) {
-                    echo "Secretaria";
-                }
-            }
-
-            ?>
+        <p class="login-box-msg">Ingresar como <?php echo $_GET["rol"]; ?>
         </p>
 
         <form method="post" style="margin-bottom: 8px;">
             <div class="form-group has-feedback">
-                <input type="email" class="form-control" name="login-user" placeholder="Usuario">
+                <input type="number" class="form-control" name="login-user" placeholder="Usuario" required>
                 <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                
             </div>
             <div class="form-group has-feedback">
-                <input type="password" class="form-control" name="login-password" placeholder="Contraseña">
+                <input type="password" class="form-control" name="login-password" placeholder="Contraseña" required>
                 <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+
             </div>
             <div class="row">
-                <!-- /.col -->
+
                 <div class="col-xs-12">
                     <button type="submit" class="btn btn-primary btn-block btn-flat">Ingresar</button>
                 </div>
-                <!-- /.col -->
+
             </div>
+
+            <?php
+
+            $login = new UsersController();
+            $login->login($_GET["rol"]);
+
+            ?>
+
         </form>
 
         <a href="#">Olvide mi contraseña</a><br>
