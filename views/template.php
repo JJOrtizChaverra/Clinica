67da949e939feae18967555bca5cebff076567ec
JJOrtizChaverra <juanjochaverra02@gmail.com>
2024-03-16 14:28:56 -0500

    Cambio en las rutas de archivos css y javascript, y agregamos el permiso de la pagina edit-profile en la condicional

--- views/template.php
+++ views/template.php
@@ -14,16 +14,16 @@ session_start();
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet" href="./views/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo Template::path(); ?>views/bower_components/bootstrap/dist/css/bootstrap.min.css">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="./views/bower_components/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo Template::path(); ?>views/bower_components/font-awesome/css/font-awesome.min.css">
     <!-- Ionicons -->
-    <link rel="stylesheet" href="./views/bower_components/Ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="<?php echo Template::path(); ?>views/bower_components/Ionicons/css/ionicons.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="./views/dist/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="<?php echo Template::path(); ?>views/dist/css/AdminLTE.min.css">
     <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="./views/dist/css/skins/_all-skins.min.css">
+    <link rel="stylesheet" href="<?php echo Template::path(); ?>views/dist/css/skins/_all-skins.min.css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -64,7 +64,8 @@ session_start();
             if (
                 $url[0] === "home" ||
                 $url[0] === "logout" ||
-                $url[0] === "profile"
+                $url[0] === "profile" ||
+                $url[0] === "edit-profile"
             ) {
                 include "modules/" . $url[0] . ".php";
             }
@@ -74,6 +75,7 @@ session_start();
 
         echo "</div>";
     } else {
+
         if (isset($_GET["url"])) {
 
             if ($_GET["url"] === "login") {
@@ -92,19 +94,19 @@ session_start();
     ?>
 
     <!-- jQuery 3 -->
-    <script src="./views/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="<?php echo Template::path(); ?>views/bower_components/jquery/dist/jquery.min.js"></script>
 
     <!-- Bootstrap 3.3.7 -->
-    <script src="./views/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="<?php echo Template::path(); ?>views/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 
     <!-- SlimScroll -->
-    <script src="./views/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+    <script src="<?php echo Template::path(); ?>views/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
     <!-- FastClick -->
-    <script src="./views/bower_components/fastclick/lib/fastclick.js"></script>
+    <script src="<?php echo Template::path(); ?>views/bower_components/fastclick/lib/fastclick.js"></script>
     <!-- AdminLTE App -->
-    <script src="./views/dist/js/adminlte.min.js"></script>
+    <script src="<?php echo Template::path(); ?>views/dist/js/adminlte.min.js"></script>
     <!-- AdminLTE for demo purposes -->
-    <script src="./views/dist/js/demo.js"></script>
+    <script src="<?php echo Template::path(); ?>views/dist/js/demo.js"></script>
     <script>
         $(document).ready(function() {
             $('.sidebar-menu').tree()
