1ca5b0f31631f45e7c9dbfce859dda1d08a9a468
JJOrtizChaverra <juanjochaverra02@gmail.com>
2024-03-16 21:25:58 -0500

    Requerimos el controlador y el modelo en el index.php

--- controllers/users.controller.php
+++ controllers/users.controller.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once "models/users.model.php";
-
 class UsersController
 {
 
--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 require_once "controllers/template.controller.php";
 
 require_once "controllers/users.controller.php";
+require_once "models/users.model.php";
+
+require_once "controllers/consulting-rooms.controller.php";
+require_once "models/consulting-rooms.model.php";
 
 $template = new Template();
 $template -> callTemplate();
