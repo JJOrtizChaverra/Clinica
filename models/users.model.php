d6adbbfd02ebc8a3de39bcba5d536d76b31016dc
JJOrtizChaverra <juanjochaverra02@gmail.com>
2024-03-16 20:49:05 -0500

    Actualizando la informacion y/o contraseña del usuario en la base de datos

--- models/users.model.php
+++ models/users.model.php
@@ -37,4 +37,52 @@ class UsersModel extends Connection
             return $e->getMessage();
         }
     }
+
+    static public function editProfile($data) {
+        try {
+            
+            $pdo = Connection::connection()->prepare("UPDATE users SET
+            name_user = :name_user, 
+            lastname_user = :lastname_user,
+            picture_user = :picture_user
+            WHERE
+            id_user = :id_user");
+
+            $pdo->bindParam(":id_user", $data["id"], PDO::PARAM_INT);
+            $pdo->bindParam(":name_user", $data["name"], PDO::PARAM_STR);
+            $pdo->bindParam(":lastname_user", $data["lastname"], PDO::PARAM_STR);
+            $pdo->bindParam(":picture_user", $data["picture"], PDO::PARAM_STR);
+
+            $pdo->execute();
+
+            $pdo = null;
+
+            return true;
+
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+    static public function changePassword($data) {
+        try {
+            
+            $pdo = Connection::connection()->prepare("UPDATE users SET
+            password_user = :password_user
+            WHERE
+            id_user = :id_user");
+
+            $pdo->bindParam(":id_user", $data["id"], PDO::PARAM_INT);
+            $pdo->bindParam(":password_user", $data["password"], PDO::PARAM_STR);
+
+            $pdo->execute();
+
+            $pdo = null;
+
+            return true;
+
+        } catch (Exception $e) {
+            return false;
+        }
+    }
 }
